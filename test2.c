3877161e7ecd808e834f308e0a1d0ab7677ece68
Juan Jose Padilla <127537907+jjp011@users.noreply.github.com>
2023-11-24 19:30:39 +0000

    test2 multichild

--- test2.c
+++ test2.c
@@ -8,17 +8,42 @@ void main(int argc, char* argv[]){
         printf("usage: %s #NUMCHILDS", argv[0]);
         exit(0);
     }
-    int pid = fork();
-    int wstatus;
 
-    if( 0 == pid){
-        long MAX_INT = 100000000000;
-        long acum = 0;
-        for(long i=0; i<MAX_INT; i++){
-            acum += 1;
-        }
-    } else {
-        printf("Waiting for child\n");
+    long MAX_INT = 10000000000;
+
+    int num_childs = atoi(argv[1]);
+    if(num_childs <= 0){
+        printf("usage: %s #NUMCHILDS", argv[0]);
+        printf("          #NUMCHILDS must be greater than 0");
+        exit(0);
     }
-    wait(&wstatus);
+    printf("launching %d childs loops\n", num_childs);
+
+    int wstatus;
+    for(int i=0; i<num_childs; i++){
+        int pid = fork();
+        if( 0 == pid){
+            printf("  Starting child %d\n", i);
+            long acum = 0;
+            for(long i=0; i<MAX_INT; i++){
+                acum += 1;
+            }
+            sleep(i);
+            printf("  End of child %d\n", i);
+            exit(0);
+        } else {
+            printf("Started child %d\n", pid);
+        }
+    }
+    sleep(1);
+    if(1 == num_childs){
+        printf("Waiting for child\n");
+    } else {
+        printf("Waiting for children\n");
+    }
+    for(int wcont=0; wcont<num_childs; wcont++){
+        int chpid = wait(&wstatus);
+        printf("Child %d exited\n", chpid);
+    }
+
 }
